85d7917d1a96a9082f62251e7927f8fdb040457a
Ezekiel Faith <ezekielfaith2001@gmail.com>
2025-03-07 19:12:45 +0100

    Merge wuth my local

--- Models/Message.js
+++ Models/Message.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import mongoose from 'mongoose';
 
 const messageSchema = new mongoose.Schema({
@@ -8,14 +8,3 @@ const messageSchema = new mongoose.Schema({
 }, { timestamps: true });
 
 export default mongoose.model('Message', messageSchema);
-=======
-import mongoose from 'mongoose';
-
-const messageSchema = new mongoose.Schema({
-  sender: { type: String, required: true },
-  receiver: { type: String, required: true },
-  content: { type: String, required: true },
-}, { timestamps: true });
-
-export default mongoose.model('Message', messageSchema);
->>>>>>> 8d98f5a (Added a dependecy for push notifcations)
